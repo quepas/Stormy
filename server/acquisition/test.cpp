43ba6836f7b39068caf9900fad23e4cb4c8235de
quepas <oneinchman.dev@gmail.com>
2013-10-10 00:38:40 +0200

    meteo stations update

--- server/acquisition/test.cpp
+++ server/acquisition/test.cpp
@@ -23,7 +23,7 @@ Out lexical_cast(In in)
 
 int main() 
 {	
-	std::cout << "Acquisition Module Test" << std::endl;
+	std::cout << "++++++++++++++++ Acquisition Module Test ++++++++++++++++" << std::endl;
 	Py_ExecutorInit();	
 	Stormy::PyParserWrapper* meteoBParser = new Stormy::PyParserWrapper("MeteoBParser");
 	Stormy::MeteoStationsCfg* meteoStationsCfg = new Stormy::MeteoStationsCfg("config/meteo_stations_config.yaml");
@@ -31,13 +31,13 @@ int main()
 	std::vector<Stormy::MeteoStation*> stations = meteoStationsCfg->getConfiguration();
 	for(auto it = stations.begin(); it != stations.end(); ++it)
 	{
-		meteoBParser->parseFromURL((*it)->url);
-	}
-
-	/*for(int i = 20; i < 40; ++i)
-	{		
-		meteoBParser -> parseFromURL(std::string("http://stacjameteo.pl/dane/index.php?stacja=") + lexical_cast<std::string>(i));
-	}*/	
+		std::cout << "=== Parsing from station " << (*it) -> name 
+			<< " ===\n\t(" << (*it) -> url << ")" << std::endl;
+		std::cout << "--------------------------------------------------------------------------" << std::endl;
+		meteoBParser -> parseFromURL((*it) -> url);
+		std::cout << "--------------------------------------------------------------------------" << std::endl;
+		std::cout << std::endl;
+	}	
 
 	getchar();
 }
