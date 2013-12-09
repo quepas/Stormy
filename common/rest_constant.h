6754719c3ebd9fe60ad9256d4b6bafe965ee4f0c
Quepas <oneinchman.dev@gmail.com>
2013-12-10 00:45:14 +0100

    Adding new rest constants.

--- common/rest_constant.h
+++ common/rest_constant.h
@@ -16,7 +16,12 @@ const std::string station_request_pattern = "/station/?";
 // api: /info
 const std::string info_request_pattern = "/info/?";
 // api query: ?param_one=value1&param_two=value2 (...)
-const std::string uri_query_vars_pattern = ".([a-zA-Z]+=[a-zA-Z0-9]+&?)*";
+const std::string uri_query_vars_pattern = ".([a-zA-Z_]+=[a-zA-Z0-9]+&?)*";
+// api available parameters
+const std::string type_parameter = "type";
+const std::string from_parameter = "from";
+const std::string to_parameter = "to";
+const std::string from_last_hours_parameter = "from_last_hours";
 
 /*
  *  JSON constants
@@ -27,6 +32,10 @@ const std::string json_name = "name";
 const std::string json_url = "url";
 const std::string json_refresh_time = "refresh_time";
 const std::string json_stations_data_marker = "stations";
+const std::string json_measurement_size_marker = "size";
+const std::string json_measurement_data_marker = "measurements";
+const std::string json_measurement_time_marker = "times";
 const std::string json_empty = "{}";
+
 // ~~ stormy::common::rest::const;
 }}}}
