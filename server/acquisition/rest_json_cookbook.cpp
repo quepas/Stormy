8edfe5765c267b3f434816ee8c71c97f33fd2650
Quepas <oneinchman.dev@gmail.com>
2013-12-29 01:51:41 +0100

    Removing unused json::cookbook recipes.

--- server/acquisition/rest_json_cookbook.cpp
+++ server/acquisition/rest_json_cookbook.cpp
@@ -21,58 +21,6 @@ namespace stormy {
     namespace json {
       namespace cookbook {
 
-string PrepareStation(entity::Station station)
-{
-	BSONObjBuilder bsonBuilder;
-	bsonBuilder
-    .append(wrapAsJSONString(db::constant::id), station.uid)
-    .append(wrapAsJSONString(db::constant::name), station.name)
-    .append(wrapAsJSONString(db::constant::url), station.url)
-	  .append(wrapAsJSONString(db::constant::refresh_time), 
-      station.refresh_time);
-	return bsonBuilder.obj().toString();
-}
-
-string cookbook::PrepareStations(const vector<entity::Station>& stations)
-{
-	string content = "{\"stations\":[";
-	Each(stations, [&](entity::Station station) {
-		content += PrepareStation(station) + ",";
-	});
-	if(stations.size() > 0)
-		content.pop_back();	// remove unnecessary comma
-	content += "]}";
-	return content;
-}
-
-string cookbook::PrepareCombinedMeasurement(
-  const std::vector<entity::Measurement>& measurement)
-{
-	BSONObjBuilder bsonBuilder;
-/*	bsonBuilder.append(wrapAsJSONString(db::constant::id),
-		ToString(measurement -> data[db::constant::mongoId]));
-	Each(measurement -> data, [&](pair<string, any> pair) {
-		if(pair.first != db::constant::mongoId) {
-			bsonBuilder.append(
-        wrapAsJSONString(pair.first), ToString(pair.second));
-		}
-	});*/
-	return bsonBuilder.obj().toString();
-}
-
-string cookbook::PrepareCombinedMeasurements(
-  const std::vector<entity::Measurement>& measurements)
-{
-	string content = "{\"meteo\":[";
-	/*Each(measurements, [&](Stormy::MeasurementPtr measurement) {
-		content += PrepareCombinedMeasurement(measurement) + ",";
-	});
-	if(measurements.size() > 0)
-		content.pop_back();	// remove unnecessary comma*/
-	content += "]}";
-	return content;
-}
-
 string cookbook::wrapAsJSONString(string label)
 {
 	return "\"" + label + "\"";
@@ -106,36 +54,6 @@ string cookbook::PrepareMetrics(const vector<entity::Metrics>& metrics_vec)
 	});
 	if(metrics_vec.size() > 0)
 		content.pop_back();	// remove unnecessary coma
-	content += "]}";
-	return content;
-}
-
-string cookbook::PrepareMeasurement(
-  const std::vector<entity::Measurement>& measurement)
-{
-	/*if(!measurement || measurement -> data.empty()) 
-    return constant::emptyJSON;
-	BSONObjBuilder bsonBuilder;
-	bsonBuilder.append(wrapAsJSONString(db::constant::timestamp),
-		NumberFormatter::format(measurement -> timestamp.epochTime()));
-	auto pair = measurement -> data.begin();
-	bsonBuilder.append(wrapAsJSONString(
-    db::constant::data), 
-    ToString(pair -> second));
-	return bsonBuilder.obj().toString();*/
-  return "{}";
-}
-
-string cookbook::PrepareMeasurements(
-  const std::vector<entity::Measurement>& measurements)
-{
-	string content = "{\"measurements\":[";
-	/*Each(measurements, [&](Stormy::MeasurementPtr measurement) {
-		if(!measurement -> data.empty())
-			content += PrepareMeasurement(measurement) + ",";
-	});
-	if(measurements.size() > 0)
-		content.pop_back();	// remove unnecessary coma*/
 	content += "]}";
 	return content;
 }
--- server/acquisition/rest_json_cookbook.h
+++ server/acquisition/rest_json_cookbook.h
@@ -11,22 +11,8 @@ namespace stormy {
     namespace json {
       namespace cookbook {
 
-std::string PrepareStation(common::entity::Station station);
-std::string PrepareStations(
-  const std::vector<common::entity::Station>& stations);
-
-std::string PrepareCombinedMeasurement(
-  const std::vector<common::entity::Measurement>& measurement);
-std::string PrepareCombinedMeasurements(
-  const std::vector<common::entity::Measurement>& measurements);
-
 std::string PrepareMetric(common::entity::Metrics metrics);
 std::string PrepareMetrics(const std::vector<common::entity::Metrics>& metrics_vec);
-
-std::string PrepareMeasurement(
-  const std::vector<common::entity::Measurement>& measurement);
-std::string PrepareMeasurements(
-  const std::vector<common::entity::Measurement>& measurements);
 
 std::string PrepareInfo(
   const std::string& server_type, 
