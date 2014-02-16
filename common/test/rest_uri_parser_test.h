bc383136263d42f912f88810f54cf23002402b03
Quepas <oneinchman.dev@gmail.com>
2014-02-16 23:12:31 +0100

    Adding URIParser query segments test case.

--- common/test/rest_uri_parser_test.h
+++ common/test/rest_uri_parser_test.h
@@ -11,6 +11,7 @@ class RestURIParserTest : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(RestURIParserTest);
     CPPUNIT_TEST(TestPathSegments);
+    CPPUNIT_TEST(TestQuerySegments);
   CPPUNIT_TEST_SUITE_END();
 public:
   void TestPathSegments()
@@ -53,5 +54,38 @@ public:
     URIParser uri_example_7("     \n\t  ");
     auto uri_7_segments = uri_example_7.getPathSegments();
     CPPUNIT_ASSERT(uri_7_segments.size() == 0);
+  }
+
+  void TestQuerySegments() 
+  {
+    URIParser uri_example_1("http://abc/aaa/bbb?ccc=123");
+    auto uri_1_queries = uri_example_1.getQuerySegments();
+    CPPUNIT_ASSERT(uri_1_queries.size() == 1);
+    CPPUNIT_ASSERT(uri_1_queries.find("ccc") != uri_1_queries.end());
+    CPPUNIT_ASSERT(uri_1_queries["ccc"] == "123");
+
+    URIParser uri_example_2("     \n\t  ");
+    auto uri_2_queries = uri_example_2.getQuerySegments();
+    CPPUNIT_ASSERT(uri_2_queries.size() == 0);
+
+    URIParser uri_example_3("   ?=af");
+    auto uri_3_queries = uri_example_3.getQuerySegments();
+    CPPUNIT_ASSERT(uri_3_queries.size() == 0);
+
+    URIParser uri_example_4("");
+    auto uri_4_queries = uri_example_4.getQuerySegments();
+    CPPUNIT_ASSERT(uri_4_queries.size() == 0);
+
+    URIParser uri_example_5("http://aaa?");
+    auto uri_5_queries = uri_example_5.getQuerySegments();
+    CPPUNIT_ASSERT(uri_5_queries.size() == 0);
+
+    URIParser uri_example_6("http://asd?=&aaa=111&bbb=&=333&ccc");
+    auto uri_6_queries = uri_example_6.getQuerySegments();
+    CPPUNIT_ASSERT(uri_6_queries.size() == 2);
+    CPPUNIT_ASSERT(uri_6_queries.find("aaa") != uri_6_queries.end());
+    CPPUNIT_ASSERT(uri_6_queries["aaa"] == "111");
+    CPPUNIT_ASSERT(uri_6_queries.find("bbb") != uri_6_queries.end());
+    CPPUNIT_ASSERT(uri_6_queries["bbb"] == "");
   }
 };
