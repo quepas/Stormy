789a79296bc8f22bffb036005d8c1dd7627967b3
Quepas <oneinchman.dev@gmail.com>
2014-05-23 00:50:45 +0200

    Moving and converting util_test.

---
+++ Server_Common_Test/src/util_test.cpp
@@ -0,0 +1,25 @@
+#include "catch.hpp"
+
+#include "util.h"
+using namespace stormy::common;
+
+TEST_CASE("conversion functions") {
+  SECTION("string to double") {
+    REQUIRE(ToDouble("123") == 123.0);
+    REQUIRE(ToDouble("abc 321abc#") == 321.0);
+    REQUIRE(ToDouble("#!#12.321abc123cba") == 12.321);
+    REQUIRE(ToDouble("abc") == 0.0);
+    REQUIRE(ToDouble(".6") == 0.6);
+    REQUIRE(ToDouble(" ") == 0.0);
+    REQUIRE(ToDouble("") == 0.0);
+  }
+
+  SECTION("string is numeric?") {
+    REQUIRE(IsNumeric("abc123.123") == false);
+    REQUIRE(IsNumeric("123") == true);
+    REQUIRE(IsNumeric("123.321") == true);
+    REQUIRE(IsNumeric(".5") == true);
+    REQUIRE(IsNumeric(" ") == false);
+    REQUIRE(IsNumeric("") == false);
+  }
+}
